c5ec09f1c7c64494301fb300be501676b5f8ebf3
Jan Philipp Berg <git.7ksst@aleeas.com>
2025-02-15 12:51:41 +0100

    EitherOr experiment

---
+++ src/main/java/net/jp/berg/EitherOr.java
@@ -0,0 +1,36 @@
+package net.jp.berg;
+
+public sealed abstract class EitherOr<T1, T2> {
+
+    public static final class Either<T1, T2> extends EitherOr<T1, T2>{
+        private final T1 VALUE;
+
+        public Either(T1 value){
+            VALUE = value;
+        }
+        public T1 get(){
+            return VALUE;
+        }
+
+    }
+
+    public static final class Or<T1, T2> extends EitherOr<T1, T2>{
+        private final T2 VALUE;
+
+        public Or(T2 value){
+            VALUE = value;
+        }
+
+        @Override
+        public T2 get() {
+            return VALUE;
+        }
+
+
+    }
+
+    public abstract Object get();
+
+
+
+}
---
+++ src/main/java/net/jp/berg/EitherOrRunner.java
@@ -0,0 +1,39 @@
+package net.jp.berg;
+
+public class EitherOrRunner
+{
+    public static void main( String[] args )
+    {
+        EitherOr<String, Integer> first = new EitherOr.Either<>("Hello");
+        EitherOr<String, Integer> second = new EitherOr.Or<>(3);
+
+        processStringOrInt(first);
+        processStringOrInt(second);
+
+        EitherOr<Float, Boolean> third = new EitherOr.Or<>(true);
+        EitherOr<Float, Boolean> fourth = new EitherOr.Either<>(17.84f);
+
+        processFloatOrBool(third);
+        processFloatOrBool(fourth);
+
+
+
+
+
+    }
+
+    public static void processStringOrInt(EitherOr<String, Integer> eo){
+
+        switch (eo){
+            case EitherOr.Either<String, Integer> either -> System.out.println("String: " + either.get());
+            case EitherOr.Or<String, Integer> or -> System.out.println("Integer calculation: " +  (or.get() + 17));
+        }
+    }
+
+    public static void processFloatOrBool(EitherOr<Float, Boolean> eo){
+        switch (eo){
+            case EitherOr.Either<Float, Boolean> either -> System.out.println("Float calculation: " + (either.get() + 18.9));
+            case EitherOr.Or<Float, Boolean> or -> System.out.println("Boolean evaluation: " +  (or.get()? "yes": "no"));
+        }
+    }
+}
